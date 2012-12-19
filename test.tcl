cc3d4a6c13808641b6e45eb6a36f6585efca0be8
Carlos Uliao <culiao@opencores.org>
2012-12-18 23:34:30 -0300

    Example now uses validation rules

--- test.tcl
+++ test.tcl
@@ -4,25 +4,36 @@ lappend auto_path [file join [pwd] lib]
 
 package require ttkform
 
-proc test1 {} {
-	toplevel .login
-	tf::Form connectForm -title " Connect to server " -submitmessage "Connect"
-	connectForm configure -rows [list \
+proc testform {} {
+	toplevel .form
+	wm title .form "Test Form"
+	
+	tf::Form dataForm -title " Try filling these fields " -submitmessage "Done"
+	dataForm configure -columns [list \
 		[list \
-			[tf::Entry formUser -title "Username" -value "user"] \
-			[tf::Entry formPass -title "Password" -show "*" -value "pass"] \
-		] \
-		[list \
-			[tf::Entry formServer -title "Server IP" -validcharacters "1234567890." -value "127.0.0.1"] \
-			[tf::Entry formPort -title "Server port" -validcharacters "1234567890" -value 21] \
+			[tf::Entry username -title "Username"] \
+			[tf::Entry password -title "Password"] \
+			[tf::Entry phone  -title "Phone number"] \
 		] \
 	]
-	connectForm configure -submitcommand { 
-		array set data [connectForm getValues] 
-		puts "\nCollected data:" 
-		parray data 
-	} 
-	pack [connectForm display .login form] -fill x -expand 1 -anchor n
+	dataForm configure -submitcommand {
+		array set data [dataForm getValues]
+		parray data
+	}
+	
+	# Form field restrictions
+	username configure -rules {{required "Username required"}} \
+		-validcharacters "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzñÑ"
+
+	password configure -show "*" \
+		-rules {{required "Password required"} \
+				{min_length 8 "Password too short"}}
+		
+	phone configure -validcharacters "1234567890-+" \
+		-rules {{required "Phone number required"}}
+	
+	# Ready to display
+	pack [dataForm display .form data] -fill x -expand 1 -anchor n -padx 10 -pady 10
 }
 
-test1
+testform
